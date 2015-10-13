e084bf1918bb478c1cf8bd811a20e7fb4869a9ba
Mikhail Yashkov <mike25@ya.ru>
2015-10-13 23:51:17 +0300

    test formatting

--- src/xlogger_test.erl
+++ src/xlogger_test.erl
@@ -6,10 +6,21 @@ test()->
 	lists:foreach(fun(X)->
 		timer:sleep(2500),
 		
-		xlogger:log(error, "Test message"),
-		xlogger:log(error, "Test Кириллица"),
-		xlogger:log(error, "Test message"),
-		xlogger:log(info, "Test info message")
+		try
+			A = 10/0
+		catch
+			Type:What->
+				xlogger:info(handler1, "That is ~p message ~p",["formatted", [{type, Type}, {what, What}, {trace, erlang:get_stacktrace()}]])
+		end,
+
+		xlogger:error(handler1, "Format numbers ~p, strings ~p, lists ~p, binaries ~p and other structures ~p",
+			[6, "some string as arg", [1,a,2,3,b,c], <<"that is binary">>, [1,{2,3,[4,{a,b},c],d}]])
+		
+		% xlogger:log(error, "Test message"),
+		% xlogger:log(error, "Test Кириллица"),
+		% xlogger:log(error, "Test message"),
+		% xlogger:log(info, "Test info message")
+
 		
 	end, lists:seq(1, 34)),
 	io:format("end testing~n"),
