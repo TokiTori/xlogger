2d790b7b9174bf467fb8adb4afc02da028fe2f66
Mikhail Yashkov <mike25@ya.ru>
2015-10-31 00:01:37 +0300

    merge xlogger.erl and xlogger_front.erl

--- src/xlogger.erl
+++ src/xlogger.erl
@@ -1,63 +1,105 @@
--module(xlogger).
+%% @author Mikhail Yashkov <mike25@ya.ru>
+%% @doc That is the interface to log messages via xlogger. 
+%% You can use strings or formatted messages with arguments to save your data to log files
+%% with specified log level and handler.
 
+-module(xlogger).
+-author("Mikhail Yashkov <mike25@ya.ru>").
+
+-behavior(gen_server).
+-export([start_link/0, init/1, handle_info/2, handle_call/3, handle_cast/2, code_change/3, terminate/2]).
 -export([
 		log/1, log/2, log/3, info/2, debug/2, warning/2, error/2, 
 		log/4, info/3, debug/3, warning/3, error/3
 	]).
 
+
 %% ===================================================================
-%% API for prepared messages
+%% External API
 %% ===================================================================
 
+start_link()->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+
+%% ===================================================================
+%% External API for prepared messages
+%% ===================================================================
+
+%% @doc Send message to default handler with 'info' log level
+%% @spec log(Msg::string()) -> ok
 log(Msg)->
 	log(info, Msg).
 
+%% @doc Send message to default handler
+%% @spec log(Level::atom(), Msg::string()) -> ok
 log(Level, Msg)->
 	log(default, Level, Msg).
 
-log(Handler, Level, Msg)->
+%% @doc Send message
+%% @spec log(HandlerName::atom(), Level::atom(), Msg::string()) -> ok
+log(HandlerName, Level, Msg)->
 	{UserModule, ExecutedModule} = get_module_name(),
-	xlogger_front:dispatch([
-		{handler, Handler}, 
+	Params = [
+		{handler, HandlerName}, 
 		{level, Level}, 
 		{msg, Msg}, 
 		{pid, self()},
 		{user_module, UserModule},
 		{module, ExecutedModule},
 		{time, calendar:local_time()}
-	]).
+	],
+	gen_server:cast(?MODULE, {log, Params}).
 
-info(Handler, Msg)->
-	log(Handler, info, Msg).
+%% @doc Send info message
+%% @spec info(HandlerName::atom(), Msg::string()) -> ok
+info(HandlerName, Msg)->
+	log(HandlerName, info, Msg).
 
-debug(Handler, Msg)->
-	log(Handler, debug, Msg).
+%% @doc Send debug message
+%% @spec debug(HandlerName::atom(), Msg::string()) -> ok
+debug(HandlerName, Msg)->
+	log(HandlerName, debug, Msg).
 
-warning(Handler, Msg)->
-	log(Handler, warning, Msg).
+%% @doc Send warning message
+%% @spec warning(HandlerName::atom(), Msg::string()) -> ok
+warning(HandlerName, Msg)->
+	log(HandlerName, warning, Msg).
 
-error(Handler, Msg)->
-	log(Handler, error, Msg).
+%% @doc Send error message
+%% @spec error(HandlerName::atom(), Msg::string()) -> ok
+error(HandlerName, Msg)->
+	log(HandlerName, error, Msg).
 
 %% ===================================================================
-%% API for format message with arguments
+%% External API for format message with arguments
 %% ===================================================================
 
-log(Handler, Level, Format, Args)->
+%% @doc Send formatted message with arguments
+%% @spec log(HandlerName::atom(), Level::atom(), Format::string(), Args::list()) -> ok
+log(HandlerName, Level, Format, Args)->
 	FormattedMessage = lists:flatten(io_lib:format(Format, Args)),
-	log(Handler, Level, FormattedMessage).
+	log(HandlerName, Level, FormattedMessage).
 
-info(Handler, Format, Args)->
-	log(Handler, info, Format, Args).
-	
-debug(Handler, Format, Args)->
-	log(Handler, debug, Format, Args).
-	
-warning(Handler, Format, Args)->
-	log(Handler, warning, Format, Args).
+%% @doc Send formatted info message with arguments
+%% @spec info(HandlerName::atom(), Format::string(), Args::list()) -> ok
+info(HandlerName, Format, Args)->
+	log(HandlerName, info, Format, Args).
 
-error(Handler, Format, Args)->
-	log(Handler, error, Format, Args).
+%% @doc Send formatted debug message with arguments
+%% @spec debug(HandlerName::atom(), Format::string(), Args::list()) -> ok
+debug(HandlerName, Format, Args)->
+	log(HandlerName, debug, Format, Args).
+
+%% @doc Send formatted warning message with arguments
+%% @spec warning(HandlerName::atom(), Format::string(), Args::list()) -> ok
+warning(HandlerName, Format, Args)->
+	log(HandlerName, warning, Format, Args).
+
+%% @doc Send formatted error message with arguments
+%% @spec error(HandlerName::atom(), Format::string(), Args::list()) -> ok
+error(HandlerName, Format, Args)->
+	log(HandlerName, error, Format, Args).
 
 %% @doc Trying to determine module which executes xlogger
 get_module_name()->
@@ -97,3 +139,30 @@ get_module_name([Head | Tail], ExcludedModules)->
 		_->
 			ModuleName
 	end.
+
+
+%% ===================================================================
+%% Gen Server callbacks
+%% ===================================================================
+
+init([])->
+	{ok, dict:new()}.
+
+handle_info(_, _State)->
+	{noreply, _State}.
+
+handle_call(_, _, _State)->
+	{reply, ok, _State}.
+
+handle_cast({log, Params}, State)->
+	xlogger_handler_sup:dispatch(Params),
+	{noreply, State};
+
+handle_cast(_, _State)->
+	{noreply, _State}.
+
+code_change(_OldVsn, _State, _Extra)->
+	{ok, _State}.
+
+terminate(_Reason, _State)->
+	ok.
