e041c3711f48c70addca15f9e14d6cc6d0f82978
Mikhail Yashkov <mike25@ya.ru>
2015-10-30 23:07:36 +0300

    updating configuration only if it changed

--- src/xlogger_configurator.erl
+++ src/xlogger_configurator.erl
@@ -11,6 +11,7 @@ start_link()->
 
 init([])->
 	Config = reload(?CONFIG_MODULE),
+	xlogger_handler_sup:apply_configuration(Config),
 	{ok, Config}.
 
 handle_info(reload, _OldConfig)->
@@ -19,6 +20,7 @@ handle_info(reload, _OldConfig)->
 			{noreply, _OldConfig};
 		NewConfig->
 			io:format("xlogger configuration changed!~n"),
+			xlogger_handler_sup:apply_configuration(NewConfig),
 			{noreply, NewConfig}
 	end.
 
